0c20375f1a5f9e029c2fa5d27fa44f0a6c9b2e1c
RJ Wright <rj@rjwright.me>
2020-11-11 16:39:36 +1100

    Update controller files to connect DB to the view.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -21,37 +21,22 @@ $(function() {
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $(".burger-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+    var newBurger = {
+      burger_name: $("#burgerField").val().trim(),
+      devoured: false
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
+        console.log(`Created a ${newBurger.burger_name}. Hope you're hungry!`);
         // Reload the page to get the updated list
         location.reload();
       }
